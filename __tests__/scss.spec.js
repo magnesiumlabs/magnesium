9e24d0c176f9c0f6943b6eae4967f7dd821069d1
Boris Damevin <borisdamevin@gmail.com>
2023-03-01 11:35:20 +0100

    test: update `scss.spec.js`

--- __tests__/scss.spec.js
+++ __tests__/scss.spec.js
@@ -6,7 +6,11 @@ const path = require('path');
 const sassTrue = require('sass-true');
 const glob = require('glob');
 
-const sassTestFiles = glob.sync(path.resolve(process.cwd(), './**/*.spec.scss'));
+const sassTestFiles = glob.sync(path.resolve(process.cwd(), './**/*.spec.scss'), {
+    ignore: [
+        '**/node_modules/**'
+    ]
+});
 
 sassTestFiles.forEach((file) => {
     sassTrue.runSass(
