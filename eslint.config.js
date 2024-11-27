a56d8fd1b98347ffa5f46f8ef3db8951a7a1de02
Boris Damevin <borisdamevin@gmail.com>
2024-11-27 11:54:39 +0100

    chore(deps): add `@front-factory/eslint-config` 1.1.0

--- eslint.config.js
+++ eslint.config.js
@@ -2,45 +2,8 @@
 //                                            ESLINT                                             //
 // ============================================================================================= //
 
-import stylistic from '@stylistic/eslint-plugin';
+import frontFactoryConfig from '@front-factory/eslint-config';
 
-export default [{
-    plugins: {
-        '@stylistic': stylistic
-    },
-    rules: {
-        'curly': [
-            'error'
-        ],
-        '@stylistic/array-bracket-newline': [
-            'error',
-            'consistent'
-        ],
-        '@stylistic/array-bracket-spacing': [
-            'error',
-            'never'
-        ],
-        '@stylistic/array-element-newline': [
-            'error',
-            'always'
-        ],
-        '@stylistic/brace-style': [
-            'error'
-        ],
-        '@stylistic/comma-spacing': [
-            'error'
-        ],
-        '@stylistic/indent': [
-            'error',
-            4
-        ],
-        '@stylistic/quotes': [
-            'error',
-            'single'
-        ],
-        '@stylistic/semi': [
-            'error',
-            'always'
-        ]
-    }
-}];
+export default [
+    ...frontFactoryConfig
+];
